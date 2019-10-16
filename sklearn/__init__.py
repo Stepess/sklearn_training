1620c16c268013869a16e4281cff893b701d1c88
Stepess <stepanersh@gmail.com>
2019-10-16 23:12:41 +0300

    added some graphics

--- sklearn/__init__.py
+++ sklearn/__init__.py
@@ -1,4 +1,26 @@
 import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
 
-df = pd.read_csv('../dataset/wine.data')
-print(df)
+class_name = 'Cultivars'
+
+
+def show_all_histograms(data):
+    for column in data.columns:
+        sns.distplot(data[column], kde=False)
+        plt.show()
+
+
+def show_all_boxplots(data: pd.DataFrame):
+    for column in data.columns:
+        if column == class_name: continue
+        sns.boxplot(x=column, y=class_name, data=df, orient='h')
+        plt.show()
+
+
+if __name__ == '__main__':
+    df = pd.read_csv('../dataset/wine.data')
+    print(df.info())
+
+    # show_all_histograms(df)
+    # show_all_boxplots(df)
