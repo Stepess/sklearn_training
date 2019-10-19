5b65e1daf2e619a0b0d5ec5165fead9739aa91c1
Stepess <stepanersh@gmail.com>
2019-10-19 13:28:53 +0300

    added knn

---
+++ sklearn_train/README.md
@@ -0,0 +1,3 @@
+kNN
+
+https://www.datacamp.com/community/tutorials/k-nearest-neighbor-classification-scikit-learn#targetText=Learn%20K%2DNearest%20Neighbor(KNN,the%20topmost%20machine%20learning%20algorithms.
--- sklearn_train/__init__.py
+++ sklearn_train/__init__.py
@@ -3,6 +3,9 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 import numpy as np
 from sklearn import preprocessing as prep
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.model_selection import train_test_split
+from sklearn import metrics
 
 class_name = 'Cultivars'
 
@@ -20,12 +23,25 @@ def show_all_boxplots(data: pd.DataFrame):
         plt.show()
 
 
+def knn_fit(data_, n, scaler_):
+    data_ = scaler_.fit_transform(np.asarray(data_))
+    x_train, x_test, y_train, y_test = train_test_split(data_, target, test_size=0.3)
+    knn = KNeighborsClassifier(n_neighbors=n)
+    knn.fit(x_train, y_train)
+    y_pred = knn.predict(x_test)
+    score = metrics.accuracy_score(y_test, y_pred)
+    print('Accuracy: {0}'.format(score))
+    return knn
+
+
 if __name__ == '__main__':
     df = pd.read_csv('../dataset/wine.data')
-    print(df.info())
+    # print(df.info())
 
     # show_all_histograms(df)
     # show_all_boxplots(df)
     scaler = prep.MinMaxScaler()
-    transform = scaler.fit_transform(np.asarray(df['Alcohol']).reshape(-1,1))
-    print(transform)
+    data = df[[x for x in df.columns if x != class_name]]
+    target = df[class_name]
+
+    knn_fit(data, 6, scaler)
